7aae3942c0e211ab9cb402fc369363a77b727ebf
MarciJoyClark <33448580+MarciJoyClark@users.noreply.github.com>
2018-05-29 17:29:59 -0700

    FINAL VERSION
    It works!

---
+++ MathOptional/Option.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathOptional
+{
+    public class Option
+    {
+        public static int option(int number)
+        {
+            int newOp = number * 10;
+            return newOp;
+        }
+        public static int option2(int optionalInt, int times = 2)
+        {
+            int newOp2 = optionalInt * times;
+            return newOp2;
+        }              
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathOptional
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("I would like 2 numbers, though I don't require the second. Enter the first, now.");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Great! Now a second (or not!)");
+            string userInput2 = Console.ReadLine();            
+                if (String.IsNullOrEmpty(userInput2))
+                {
+                    int variable = Option.option(userInput);
+                    Console.WriteLine("Your first number times 10 = " + variable + ".");
+                }
+                else
+                {
+                    int variable = Option.option(userInput);
+                    int userInput3 = Convert.ToInt32(userInput2);
+                    int variable2 = Option.option2(userInput3);
+                    Console.WriteLine("Your first number times 2 = " + variable + ".  Your second times 2 = " + variable2 + ".");
+                }
+                Console.ReadLine();            
+        }
+    }
+}
